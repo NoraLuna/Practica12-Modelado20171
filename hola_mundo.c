569ab4c94f6c60994edc5996f53e326722b1b257
NoraLuna <nora-hdz@ciencias.unam.mx>
2016-10-31 13:35:25 -0600

    agregue el archivo hola_mundo

---
+++ hola_mundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //incluimos la biblioteca para poder imprimir y leer de consola
+
+int main() //declaro la funcion principal
+{
+	printf("¡Hola, Mundo!"); //imprimo la cadena hola mundo
+	return 0; //regreso 0
+}
